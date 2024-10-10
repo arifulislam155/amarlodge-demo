ab13f2b34de50c5ec9e12e288affb4dc3fa3bae3
arifulislam155 <arifulislam155@gmail.com>
2024-10-10 23:26:47 +0600

    first commit

---
+++ init/data.js
@@ -0,0 +1,85 @@
+const sampleListings = [
+    {
+      title: "Cozy Beachfront Cottage",
+      description: "Escape to this charming beachfront cottage in Malibu.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 1500,
+      location: "Malibu",
+      country: "United States"
+    },
+    {
+      title: "Luxury Mountain Cabin",
+      description: "Experience tranquility at this luxury cabin in the mountains.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 2500,
+      location: "Aspen",
+      country: "United States"
+    },
+    {
+      title: "Urban Loft Apartment",
+      description: "Stay in the heart of the city with this modern loft apartment.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 1800,
+      location: "New York",
+      country: "United States"
+    },
+    {
+      title: "Rustic Forest Cabin",
+      description: "Cozy up in this secluded forest cabin for a nature retreat.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 1200,
+      location: "Portland",
+      country: "United States"
+    },
+    {
+      title: "Charming Lakehouse",
+      description: "Relax by the lake in this beautiful lakeside home.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 2000,
+      location: "Lake Tahoe",
+      country: "United States"
+    },
+    {
+      title: "Modern Downtown Condo",
+      description: "Stylish downtown condo with panoramic city views.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 2200,
+      location: "Chicago",
+      country: "United States"
+    },
+    {
+      title: "Sunny Beachside Villa",
+      description: "Enjoy the ocean breeze in this sunny beachside villa.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 2800,
+      location: "Miami",
+      country: "United States"
+    },
+    {
+      title: "Historic Castle Stay",
+      description: "Live like royalty in this historic castle nestled in the countryside.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 5000,
+      location: "Edinburgh",
+      country: "Scotland"
+    },
+    {
+      title: "Ski Chalet Retreat",
+      description: "Perfect retreat for skiers, located close to slopes with beautiful views.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 3200,
+      location: "Zermatt",
+      country: "Switzerland"
+    },
+    {
+      title: "Countryside Farmhouse",
+      description: "Escape the hustle and bustle in this peaceful countryside farmhouse.",
+      image: "https://images.unsplash.com/photo-1720048170970-3848514c3d60?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      price: 1600,
+      location: "Tuscany",
+      country: "Italy"
+    }
+  ];
+  
+
+  module.exports = { data: sampleListings};
